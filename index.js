48682dcb453e0d5baf76d44173262a8f9a449957
yy0234 <yunglp0234@gmail.com>
2017-12-21 23:41:47 +0800

    modify index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 var pg = require('pg');
 
-//app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
