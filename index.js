42a937f26e7e5a5be9bc8c073b6a9498da6a2da3
yy0234 <yunglp0234@gmail.com>
2017-12-21 23:44:39 +0800

    modify index.js

--- index.js
+++ index.js
@@ -8,18 +8,18 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
-//app.set('views', __dirname + '/views');
-//app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
 
 
 app.get('/', function(request, response) {
-  response.render('index.html')
+  response.sendFile('index')
 });
 
 
-/*app.get('/login', function(request, response) {
-  response.sendFile('pages/login.html')
-});*/
+app.get('/login', function(request, response) {
+  response.sendFile('login')
+});
 
 app.get('/cool', function(request, response) {
   response.send(cool());
