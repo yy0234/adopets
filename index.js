5fd65f0032328b3765c56a1ff16e87163be907a8
yy0234 <yunglp0234@gmail.com>
2018-01-11 19:16:38 +0800

    add index page

--- index.js
+++ index.js
@@ -80,6 +80,10 @@ app.get("/getUserData", function (request, response) {
   });
 });
 
+app.get('*', function(request, response) {
+  response.render('pages/index');
+});
+
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
