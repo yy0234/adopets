e1676f2bfa68576d433348b0211e9dbf8831e51d
yy0234 <yunglp0234@gmail.com>
2018-02-14 23:43:28 +0800

    debug

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.get('/photoSearch_notfinished', function(request, response) {
 
 app.get('/db', function (request, response) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM user_table', function(err, result) {
+    client.query('SELECT * FROM users', function(err, result) {
       done();
       if (err)
        { console.error(err); response.send("Error " + err); }
