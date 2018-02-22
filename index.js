d230c6031eae3da03b2eb0e6c4d21a0a8116ba7e
yy0234 <yunglp0234@gmail.com>
2018-02-22 13:31:26 +0800

    try image serach

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ app.get('/testing_base64', function(request, response) {
   // Covert the image data to a Buffer and base64 encode it.
   var encoded = new Buffer(imageFile).toString('base64');
 
-  return response.send(encoded);
+  return encoded;
 });
 
 app.get('/db', function (request, response) {
