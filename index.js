07c2df3165b99da299de8b84a5ad514599f4c8a7
Andy <tat1996@hotmail.com>
2018-03-15 01:18:19 +0800

    test

--- index.js
+++ index.js
@@ -151,7 +151,7 @@ app.post('/addSupply', function (request, response) {
 
 app.get("/listSupply", function (request, response) { 
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-	   client.query("SELECT * FROM petsupply"+ request.query.query, function(err, result) {
+	   client.query("SELECT * FROM petsupply "+ request.query.query, function(err, result) {
        done();
        if (err)
         { console.error(err); return response.send("Error " + err); }
