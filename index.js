8b7ec0292adc47d50b6a855ae6e3467986fbb6fd
Andy <tat1996@hotmail.com>
2018-03-15 00:48:09 +0800

    shop test

--- index.js
+++ index.js
@@ -151,7 +151,7 @@ app.post('/addSupply', function (request, response) {
 
 app.get("/listSupply", function (request, response) { 
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-	   client.query("SELECT * FROM petsupply WHERE price IS NOT NULL"+ request.query.query+" order by postdate DESC", function(err, result) {
+	   client.query("SELECT * FROM petsupply WHERE price IS NOT NULL"+ request.query.query, function(err, result) {
        done();
        if (err)
         { console.error(err); return response.send("Error " + err); }
