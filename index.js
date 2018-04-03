7dfee46e325ff34181133ca6224582a225cf180f
yy0234 <yunglp0234@gmail.com>
2018-04-03 17:31:28 +0800

    I don't know

--- index.js
+++ index.js
@@ -70,14 +70,39 @@ app.get('/run_cat_scraper', function(request, response) {
     if (!error) {
       var $ = cheerio.load(body);
       var data = $("[class='textdb12pt']>span");   
+      var photo=$("[class='photoborder']");
       for (var x=0; x<(data.length/5);x++){
         var pos=x*5;
         var obj={};
+        var temp=(photo[x].attribs.src).replace("./","http://www.lap.org.hk/");
+        obj['photo']=temp;
         obj['name']=data[pos].children[0].data;
         obj['breed']=data[pos+1].children[0].data;
         obj['age']=data[pos+2].children[0].data;
         obj['sex']=data[pos+3].children[0].data;
-        obj['detail']=data[pos+4].children[0].data;
+        returnList.push(obj);
+      }
+    }
+  });
+  response.send(returnList);
+});
+
+app.get('/run_dog_scraper', function(request, response) {
+  var returnList=[];
+  req("http://www.lap.org.hk/adoptdog.aspx", function (error,r, body) {
+    if (!error) {
+      var $ = cheerio.load(body);
+      var data = $("[class='textdb12pt']>span");   
+      var photo=$("[class='photoborder']");
+      for (var x=0; x<(data.length/5);x++){
+        var pos=x*5;
+        var obj={};
+        var temp=(photo[x].attribs.src).replace("./","http://www.lap.org.hk/");
+        obj['photo']=temp;
+        obj['name']=data[pos].children[0].data;
+        obj['breed']=data[pos+1].children[0].data;
+        obj['age']=data[pos+2].children[0].data;
+        obj['sex']=data[pos+3].children[0].data;
         returnList.push(obj);
       }
     }
--- myscraper/scraper.js
+++ myscraper/scraper.js
@@ -1,7 +1,7 @@
 var req = require("request");
 var cheerio = require("cheerio");
 
-var url = "http://www.lap.org.hk/adoptcat.aspx";
+var url = "http://www.lap.org.hk/adoptdog.aspx";
 
 // 取得網頁資料
 req(url, function (error, response, body) {
@@ -10,16 +10,18 @@ req(url, function (error, response, body) {
     var $ = cheerio.load(body);
 
     var data = $("[class='textdb12pt']>span");
+	var photo=$("[class='photoborder']");
 	var returnList=[];
 
 	for (var x=0; x<(data.length/5);x++){
 		var pos=x*5;
 		var obj={};
+		var temp=(photo[x].attribs.src).replace("./","http://www.lap.org.hk/");
+		obj['photo']=temp;
 		obj['name']=data[pos].children[0].data;
 		obj['breed']=data[pos+1].children[0].data;
 		obj['age']=data[pos+2].children[0].data;
 		obj['sex']=data[pos+3].children[0].data;
-		obj['detail']=data[pos+4].children[0].data;
 		returnList.push(obj);
 	}
 		
