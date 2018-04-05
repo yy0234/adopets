0a2225c5b88c32737af1cd87253971fa649930ed
yy0234 <yunglp0234@gmail.com>
2018-04-06 01:01:37 +0800

    debug

--- index.js
+++ index.js
@@ -158,7 +158,7 @@ app.get('/regist', function (request, response) {
 
 app.get("/signin", function (request, response) { 
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-	  var sql = "SELECT * FROM users WHERE userid IN (" + request.body.username + ") AND password IN (" + request.body.password+")"; 
+	  var sql = "SELECT * FROM users WHERE userid IN " + request.query.username + " AND password IN " + request.query.password; 
 	  client.query(sql, function(err, result) {
        done();
        if (err)
