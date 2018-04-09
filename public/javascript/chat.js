5e11ec7fc452b5f06653a861cd642f3aab2463a2
Andy <tat1996@hotmail.com>
2018-04-10 00:25:20 +0800

    testing

--- public/javascript/chat.js
+++ public/javascript/chat.js
@@ -106,18 +106,6 @@ HiChat.prototype = {
                 reader.readAsDataURL(file);
             };
         }, false);
-        this._initialEmoji();
-        document.getElementById('emoji').addEventListener('click', function(e) {
-            var emojiwrapper = document.getElementById('emojiWrapper');
-            emojiwrapper.style.display = 'block';
-            e.stopPropagation();
-        }, false);
-        document.body.addEventListener('click', function(e) {
-            var emojiwrapper = document.getElementById('emojiWrapper');
-            if (e.target != emojiwrapper) {
-                emojiwrapper.style.display = 'none';
-            };
-        });
         document.getElementById('emojiWrapper').addEventListener('click', function(e) {
             var target = e.target;
             if (target.nodeName.toLowerCase() == 'img') {
@@ -126,17 +114,6 @@ HiChat.prototype = {
                 messageInput.value = messageInput.value + '[emoji:' + target.title + ']';
             };
         }, false);
-    },
-    _initialEmoji: function() {
-        var emojiContainer = document.getElementById('emojiWrapper'),
-            docFragment = document.createDocumentFragment();
-        for (var i = 69; i > 0; i--) {
-            var emojiItem = document.createElement('img');
-            emojiItem.src = '../content/emoji/' + i + '.gif';
-            emojiItem.title = i;
-            docFragment.appendChild(emojiItem);
-        };
-        emojiContainer.appendChild(docFragment);
     },
     _displayNewMsg: function(user, msg, color) {
         var container = document.getElementById('historyMsg'),
@@ -157,20 +134,5 @@ HiChat.prototype = {
         msgToDisplay.innerHTML = user + '<span class="timespan">(' + date + '): </span> <br/>' + '<a href="' + imgData + '" target="_blank"><img src="' + imgData + '"/></a>';
         container.appendChild(msgToDisplay);
         container.scrollTop = container.scrollHeight;
-    },
-    _showEmoji: function(msg) {
-        var match, result = msg,
-            reg = /\[emoji:\d+\]/g,
-            emojiIndex,
-            totalEmojiNum = document.getElementById('emojiWrapper').children.length;
-        while (match = reg.exec(msg)) {
-            emojiIndex = match[0].slice(7, -1);
-            if (emojiIndex > totalEmojiNum) {
-                result = result.replace(match[0], '[X]');
-            } else {
-                result = result.replace(match[0], '<img class="emoji" src="../content/emoji/' + emojiIndex + '.gif" />');//todo:fix this in chrome it will cause a new request for the image
-            };
-        };
-        return result;
     }
 };
