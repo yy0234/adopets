1aa4bf3127eb3471c4480c738aba0555254c7030
yy0234 <yunglp0234@gmail.com>
2018-04-20 20:19:39 +0800

    ...

--- index.js
+++ index.js
@@ -636,7 +636,7 @@ app.post('/addSupply', function (request, response) {
 
 app.get("/listSupply", function (request, response) { 
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-	   client.query("SELECT * FROM petsupply "+ request.query.query, function(err, result) {
+	   client.query("SELECT * FROM petsupply "+ request.query.query +" order by supplyid DESC", function(err, result) {
        done();
        if (err)
         { console.error(err); return response.send("Error " + err); }
